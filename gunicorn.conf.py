cc2c20d3dce5800935957d2654af8169c1d59dda
cleanmountain <andreas.reenberg.kjaer@gmail.com>
2024-02-16 21:44:17 +0100

    auto open browser on launch

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,15 @@
+import webbrowser
 from get_ip import get_local_ip
 
+# gunicorn config
 _host = get_local_ip()
 _port = 6969
 bind = f"{_host}:{_port}"
 workers = 2
 loglevel="warning"
 
+# user facing, non-config
 _green = '\033[92m'
 _nc = '\033[0m'
 print(_green + f"FILECANNON IS NOW ACCESSIBLE AT http://{bind}" + _nc)
-print(_green + f"FILECANNON IS NOW ACCESSIBLE AT http://{bind}" + _nc)
-print(_green + f"FILECANNON IS NOW ACCESSIBLE AT http://{bind}" + _nc)
+webbrowser.open(f"http://{bind}")
