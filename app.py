0867b24af492e7fcd49f179efba811d0e9129e0b
cleanmountain <andreas.reenberg.kjaer@gmail.com>
2024-02-16 21:22:37 +0100

    Change upload from binary data in db to fileupload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from io import BytesIO
+import uuid
 import datetime
+from pathlib import Path
 from flask import Flask, render_template, request, send_file, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import qrcode
 from get_ip import get_local_ip
-
 
 os.chdir(os.path.dirname(__file__))
 
@@ -13,18 +13,19 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 app.secret_key = "donkey"
+app.config["UPLOAD_FOLDER"] = "static/files"
 
 db = SQLAlchemy(app)
 
 class Upload(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     filename = db.Column(db.String(50))
-    timestamp = db.Column(db.String(19))
-    data = db.Column(db.LargeBinary)
+    timestamp = db.Column(db.String(20))
+    path = db.Column(db.String(255))
+
 
 @app.before_first_request
 def my_func():
-    # host = request.host
     host = get_local_ip()
     url = f"http://{host}/"
     make_shortcut_qr(url)
@@ -45,11 +46,15 @@ def upload():
         
         if not files[0].filename:
             flash("No file was submitted.", "error")
-            return redirect(url_for("upload"))
+            return redirect(url_for("upload"))            
 
         for file in files:
             upload_count += 1
-            upload = Upload(filename=file.filename, data=file.read(), timestamp=get_time())
+            upload_id = str(uuid.uuid4())
+            Path(app.config["UPLOAD_FOLDER"]).mkdir(parents=True, exist_ok=True)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], upload_id))
+            path = f"{app.config['UPLOAD_FOLDER']}/{upload_id}"
+            upload = Upload(id=upload_id, filename=file.filename, timestamp=get_time(), path=path)
             db.session.add(upload)
             db.session.commit()
         
@@ -63,22 +68,30 @@ def upload():
 @app.route("/download/<upload_id>")
 def download(upload_id):
     upload = Upload.query.filter_by(id=upload_id).first()
-    return send_file(BytesIO(upload.data), download_name=upload.filename, as_attachment=True)
+    return send_file(upload.path, download_name=upload.filename, as_attachment=True)
 
 
 @app.route("/clear")
 def clear():
+    uploads = Upload.query.order_by(Upload.id.desc()).all()
+    
+    for upload in uploads:
+        os.remove(os.path.join(upload.path))
+
     Upload.query.delete()
     db.session.commit()
     return redirect(url_for("uploads_overview"))
+
 
 @app.route("/access")
 def access():
     return render_template("access.html")
 
+
 def get_time() -> str:
     time: str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return time
+
 
 def make_shortcut_qr(url) -> None:
     qr = qrcode.QRCode(border=4)
